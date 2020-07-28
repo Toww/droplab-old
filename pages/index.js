aba74b48dc145e10ad7789388d395cfa41000114
finn <25932452+Toww@users.noreply.github.com>
2020-07-28 18:42:59 +0200

    Homepage project lists OK

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
-import Link from 'next/link';
+import Link from "next/link";
+import { getSortedProjectsData } from "../lib/projects";
 
-export default function Home() {
+export default function Home({ allProjectsData }) {
   return (
     <div className="container">
       <Head>
@@ -11,11 +12,32 @@ export default function Home() {
 
       <main>
         <h1>This is the homepage, yay! </h1>
-        <p className="text-orange-500">Orange <span>blue</span></p>
+        <p className="text-orange-500">
+          Orange <span>blue</span>
+        </p>
         <Link href="/about">
           <a>About page</a>
         </Link>
+        <section>
+          <h2>Projects</h2>
+          <ul>
+            {allProjectsData.map(({ id, date, title }) => (
+              <li key={id}>
+                {title} - {date}
+              </li>
+            ))}
+          </ul>
+        </section>
       </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const allProjectsData = getSortedProjectsData();
+  return {
+    props: {
+      allProjectsData,
+    },
+  };
 }
---
+++ pages/projects/[id].js
@@ -0,0 +1,9 @@
+
+
+export const Project = () => {
+    return (  );
+}
+
+export async function getStaticPaths() {
+    // Return a list of possible value for id
+  }
