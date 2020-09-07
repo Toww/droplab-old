28d99589e70e5e60fc64957dcea9702e50ec1ca6
Toww <25932452+Toww@users.noreply.github.com>
2020-09-07 17:29:00 +0200

    added two decks player

--- pages/index.js
+++ pages/index.js
@@ -8,18 +8,7 @@ export default function Home({ allProjectsData }) {
 
       <header>
         <h1 className="text-white text-center text-3xl leading-tight">
-          Hello! My name is{" "}
-          <span className="font-custom text-4xl text-red-400 tracking-wide leading-tight">
-            Thomas,
-          </span>{" "}
-          I am a <span className="font-custom text-4xl text-red-400 tracking-wide leading-tight">
-            front-end developer
-          </span>{" "}
-          and used to be a{" "}
-          <span className="font-custom text-4xl text-red-300 tracking-wide leading-tight">
-            graphic designer
-          </span>
-          . Here is a selection of my work:
+          Hello! My name is <span className="font-custom text-4xl text-red-400 tracking-wide leading-tight">Thomas</span>, I am a <span className="font-custom text-4xl text-red-400 tracking-wide leading-tight"> front-end developer</span> and used to be a <span className="font-custom text-4xl text-red-300 tracking-wide leading-tight"> graphic designer</span>. Here is a selection of my work:
         </h1>
       </header>
       <main>
