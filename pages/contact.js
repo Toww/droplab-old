01990656f57926e21aff43a8dc11b382258d8364
Toww <25932452+Toww@users.noreply.github.com>
2020-09-16 12:57:26 +0200

    Solved accessibility issues

--- pages/contact.js
+++ pages/contact.js
@@ -22,16 +22,28 @@ const About = () => {
             method="POST"
           >
             <div className="inline-block md:w-1/2 md:pr-4">
-              <label className="block text-orange-300 text-lg mb-2" htmlFor="name">
+              <label
+                className="block text-orange-300 text-lg mb-2"
+                htmlFor="name"
+              >
                 Name
               </label>
-              <input required className="mb-8"></input>
+              <input required className="mb-8" id="name" name="name"></input>
             </div>
             <div className="inline-block md:w-1/2 md:pl-4">
-              <label className="w-100 block text-orange-300 text-lg mb-2" htmlFor="name">
+              <label
+                className="w-100 block text-orange-300 text-lg mb-2"
+                htmlFor="email"
+              >
                 Email
               </label>
-              <input required className="mb-8" type="email" name="email"></input>
+              <input
+                required
+                className="mb-8"
+                type="email"
+                name="email"
+                id="email"
+              ></input>
             </div>
             <div>
               <label
@@ -41,14 +53,19 @@ const About = () => {
                 Message
               </label>
               <textarea
-              className="appearance-none resize-none h-32 rounded w-full py-2 px-3 text-gray-700 mb-8"
+                className="appearance-none resize-none h-32 rounded w-full py-2 px-3 text-gray-700 mb-8"
                 required
                 type="text"
                 name="message"
                 id="message"
               ></textarea>
             </div>
-            <button className="block w-1/2 mx-auto border-2 text-bold border-orange-300 text-orange-300 hover:bg-orange-300 hover:text-gray-800 rounded py-4 px-8" type="submit">Send</button>
+            <button
+              className="block w-1/2 mx-auto border-2 text-bold border-orange-300 text-orange-300 hover:bg-orange-300 hover:text-gray-800 rounded py-4 px-8"
+              type="submit"
+            >
+              Send
+            </button>
           </form>
         </section>
       </main>
